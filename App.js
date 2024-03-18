05659406a7db9f29df229ca1636c40a818238e04
BURmister <olegcernysov903@gmail.com>
2024-03-18 23:11:43 +0300

    fix bug

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
+import { useRef } from 'react';
+import { StyleSheet, Animated, View, useColorScheme, useWindowDimensions } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+const CURSOR_SIDE_SIZE = 20;
+const CURSOR_HALF_SIDE_SIZE = CURSOR_SIDE_SIZE / 2;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const colorScheme = useColorScheme();
+
+   const { height, width } = useWindowDimensions();
+   const touch = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+
+   return (
+      <View
+         onStartShouldSetResponder={() => true}
+         onMoveShouldSetResponder={() => true}
+         onResponderMove={(event) => {
+            touch.setValue({
+               x: event.nativeEvent.pageX,
+               y: event.nativeEvent.pageY,
+            });
+            console.log({
+               x: event.nativeEvent.pageX,
+               y: event.nativeEvent.pageY,
+            });
+         }}
+         onResponderRelease={() => {
+            Animated.spring(touch, {
+               toValue: {
+                  x: width / 2 + CURSOR_HALF_SIDE_SIZE,
+                  y: height / 2 + CURSOR_HALF_SIDE_SIZE,
+               },
+               useNativeDriver: false,
+            }).start();
+         }}
+         style={{ flex: 1 }}>
+         <Animated.View
+            style={{
+               position: 'absolute',
+               top: Animated.subtract(touch.y, CURSOR_HALF_SIDE_SIZE),
+               left: Animated.subtract(touch.x, CURSOR_HALF_SIDE_SIZE),
+               // top: height / 2 - CURSOR_HALF_SIDE_SIZE,
+               // left: width / 2 - CURSOR_HALF_SIDE_SIZE,
+               height: CURSOR_SIDE_SIZE,
+               width: CURSOR_SIDE_SIZE,
+               borderRadius: CURSOR_HALF_SIDE_SIZE,
+               backgroundColor: 'skyblue',
+            }}
+         />
+      </View>
+   );
+}
+
+{
+   /* <View style={[styles.container, { backgroundColor: colorScheme === 'light' ? '#fff' : '#000' }]}>
+   <Text style={{ color: colorScheme === 'light' ? '#000' : '#fff' }}>Open up App.js to start working on your app! </Text>
+   <StatusBar style="auto" />
+</View> */
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      // backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
